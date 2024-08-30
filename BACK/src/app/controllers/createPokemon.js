49958d1a410f92dd342ebd947549a895804f75cb
Joe1Vieira <vcjoel10@gmail.com>
2024-08-30 10:50:15 -0300

    alterações

--- BACK/src/app/controllers/createPokemon.js
+++ BACK/src/app/controllers/createPokemon.js
@@ -5,6 +5,14 @@ const createPokemon = async (req, res) => {
     const { name, number, type, img_url } = req.body
 
     try {
+        const existingPokemon = await Pokemon.findOne({
+            $or: [{name}, {number}]
+        })
+
+        if (existingPokemon) {
+            return res.status(400).json({error: "Pokkemon with same name or number alredy exist"})
+        }
+
         const pokemon = await Pokemon.create({ name, number, type, img_url })
         res.status(200).json(pokemon);
     } catch (error) {
--- BACK/src/app/controllers/getPokemon.js
+++ BACK/src/app/controllers/getPokemon.js
@@ -2,7 +2,7 @@ const Pokemon = require('../models/pokemon')
 
 const getPokemon = async (req, res) => {
 
-    const pokemon = await Pokemon.find({}).sort({createdAt: -1})
+    const pokemon = await Pokemon.find({})
 
     res.status(200).json(pokemon)
 }
